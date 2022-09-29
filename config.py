a0ae13a6a367c7c13109ed8830d84c96a1fdcd9e
Punitha <punithak2412@gmail.com>
2022-09-29 12:28:27 +0530

    deploy

--- config.py
+++ config.py
@@ -3,7 +3,9 @@ from pydantic import BaseSettings
 class Setting(BaseSettings):
     db_password : str
     db_name : str
-
+    db_host : str
+    db_port : int
+    db_user : str
     class Config:
         env_file = '.env'
 
--- database.py
+++ database.py
@@ -3,7 +3,7 @@ from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 from config import settings
 
-SQLALCHEMY_DATABASE_URL = f'postgresql://Punitha:{settings.db_password}@localhost/{settings.db_name}'
+SQLALCHEMY_DATABASE_URL = f'postgresql://{settings.db_user}:{settings.db_password}@{settings.db_host}/{settings.db_name}'
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
